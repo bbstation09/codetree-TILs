51b0907422197c4aba0d6446c7c5f3e09d38bcc0
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-22 12:40:38 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/데이터 교환/exchange-data.cpp
+++ 240722/데이터 교환/exchange-data.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a=5,b=6,c=7,tmp=0;
-    tmp=b;
-    b=a;
-    a=c;
-    c=tmp;
+
+    // b에는 a값을 , c에는 b값을, a에는 c값을 넣어 출력
+    tmp = a;
+    a = c;
+    c = b;
+	b = tmp;
     cout<<a<<endl<<b<<endl<<c;
     return 0;
 }
