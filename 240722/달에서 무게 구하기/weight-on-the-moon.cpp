0c9395d37df5ff18a12a335a721187089a4e74fe
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-22 11:42:24 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    int choo_weight=13;
+    double moon_weight=0.165;
+
+    cout<<fixed;
+    cout.precision(6);
+
+    cout<<choo_weight<<" * "<<moon_weight<<" = "<<choo_weight*moon_weight;
+    return 0;
+}
