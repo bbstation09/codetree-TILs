1b7358bfee5a107df19cf6249202ddd6bca0d8cb
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-24 13:13:34 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int yyyy,mm,dd;
+
+    cin>>mm;
+    cin.get();
+    cin>>dd;
+    cin.get();
+    cin>>yyyy;
+
+    cout<<yyyy<<"."<<mm<<"."<<dd;
+
+    return 0;
+}
