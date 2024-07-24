c10393ec4668ea56eaecf2a6721412422680a3cc
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-24 13:37:42 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/전화번호 바꾸기/change-phone-number.cpp
+++ 240724/전화번호 바꾸기/change-phone-number.cpp
@@ -3,17 +3,16 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    string s;
-    int y,z;
+    string phoneNumber;
+    cin >> phoneNumber;
 
-    cin>>s; // 010
-    cin.get(); // -
-    cin>>y;
-    cin.get();
-    cin>>z;
+    // 전화번호를 하이픈을 기준으로 나누기
+    string prefix = phoneNumber.substr(0, 4); // "010-"
+    string xxxx = phoneNumber.substr(4, 4);   // "1234"
+    string yyyy = phoneNumber.substr(9, 4);   // "5678"
 
-    cout<<s<<"-"<<z<<"-"<<y;
-    
+    // 형식에 맞게 출력
+    cout << prefix << yyyy << "-" << xxxx << endl;
+
     return 0;
 }
