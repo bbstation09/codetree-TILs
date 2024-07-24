99a1596439616bf573580907d4608ca2285a2d21
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-24 13:17:53 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int first,last;
+    cin>>first;
+    cin.get();
+    cin>>last;
+
+    cout<<first<<last;
+
+    return 0;
+}
