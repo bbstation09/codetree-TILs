5292da31a02a44c9290ac3e6f16319a18dd00b89
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-24 18:33:56 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,sum;
+    double avr;
+    cin>>a>>b;
+    sum=a+b;
+    avr = (double)(a+b)/2;
+    cout<<sum<<" "<<avr;
+    return 0;
+}
