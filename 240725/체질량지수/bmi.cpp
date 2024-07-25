2d58a2d5d4c78b4c76c1061e3352d4f736887834
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-25 16:26:48 +0900

    Title: 체질량지수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/체질량지수/bmi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int height, weight, bmi;
+    string state;
+
+    cin>>height>>weight;
+
+    bmi=(10000*weight)/(height*height);
+
+    if(bmi>=25){
+        state="Obesity";
+        cout<<bmi<<endl<<state;
+    }else{
+        cout<<bmi;
+    }
+
+    return 0;
+}
