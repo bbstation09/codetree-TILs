3b019e855fe231eca7d6c05980d2d8f93151de8d
chaewonkim <100389663+bbstation09@users.noreply.github.com>
2024-07-26 13:58:21 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 240726/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -3,19 +3,13 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,b,c,min_val;
+    int a,b,c;
     cin>>a>>b>>c;
 
     if(a <= b && a <= c)
 		cout << "1 ";
 	else
 		cout << "0 ";
-
-    if(a==min_val){
-        cout<<"1 ";
-    }else{
-        cout<<"0 ";
-    }
 
     if(a==b && b==c){
         cout<<"1";
